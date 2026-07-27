d0a3b27cc97154194533c05b72373f937e44a132
Rishabh Singh <20rishabh05@gmail.com>
2026-07-28 00:29:17 +0530

    Remove SecurityConfig and add AuthResponse and LoginRequest DTOs for authentication

---
+++ backend/src/main/java/com/rishabh/game_platform/auth/api/dto/LoginRequest.java
@@ -0,0 +1,17 @@
+package com.rishabh.game_platform.auth.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class LoginRequest {
+    
+    private String username;
+    private String password;
+}
